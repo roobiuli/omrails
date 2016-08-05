6fbf797306ce72ffb2219c3454f8d438c7c2e502
robert iulian <nicoara.robert@gmail.com>
2016-08-05 17:32:18 +0300

    Layouts and HTML pages add

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'index' => 'pages#index'
   get 'test' => 'pages#test'
+  get 'about' => 'pages#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#home"
